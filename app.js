2ee1cf9423cefd77c1745efdcec3e0d4718e74f5
Ashley Hunt <ashley.hunt1992@gmail.com>
2024-06-26 17:41:55 +0100

    Remove logger

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const { verifyJWT } = require("./controllers/authController")
 var app = express()
 
 app.use(cors(require("./config/corsOptions")))
-app.use(logger("dev"))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
